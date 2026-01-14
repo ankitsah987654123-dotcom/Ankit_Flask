0097a987836fb7b150b7803db6d3d72dc5622b30
ankitsah987654123-dotcom <ankitsah987654123@gmail.com>
2026-01-14 20:57:31 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template, request
+import joblib
+import numpy as np
+app = Flask(__name__)
+model = joblib.load("student_pass_model.pkl")
+@app.route("/", methods=["GET", "POST"])
+def home():
+ prediction = None
+ study_hours = ""
+ attendance = ""
+ if request.method == "POST":
+  study_hours = request.form["study_hours"]
+  attendance = request.form["attendance"]
+  pred = model.predict(np.array([[float(study_hours),float(attendance)]]))[0]
+  prediction = "PASS" if pred == 1 else "Fail"
+ return render_template("index.html",
+ prediction=prediction,
+ study_hours=study_hours,
+ attendance=attendance)
+if __name__ == "__main__":
+ app.run(host="0.0.0.0", port=7860)
